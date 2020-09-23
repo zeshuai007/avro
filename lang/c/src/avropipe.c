e84427adafcf08f1cb41a76d28c878b3a3066066
zeshuai007 <51382517@qq.com>
2020-09-23 11:10:33 +0800

    test in windows avromod avropipe

--- lang/c/src/avropipe.c
+++ lang/c/src/avropipe.c
@@ -383,10 +383,6 @@ process_file(const char *filename)
 
 
 /*-- MAIN PROGRAM --*/
-static struct option longopts[] = {
-	{ "separator", required_argument, NULL, 's' },
-	{ NULL, 0, NULL, 0 }
-};
 
 static void usage(void)
 {
@@ -400,33 +396,52 @@ int main(int argc, char **argv)
 {
 	char  *data_filename;
 
-	int  ch;
-	while ((ch = getopt_long(argc, argv, "s:", longopts, NULL) ) != -1) {
-		switch (ch) {
-			case 's':
-				separator = optarg;
-				break;
-
-			default:
-				usage();
-				exit(1);
+	int i = 1;
+	int ind_point = 0;
+	int ind = 0;
+	char *p_str;
+	while (i < argc) {
+	    if (**(argv+i) == '-') {
+                if (*(*(argv+i)+1) == '-') {
+                    if (strncmp("separator", *(argv+i)+2, strlen("separator")) == 0) {
+                        p_str = strchr(*(argv+i)+2, '=');
+			if (p_str != NULL) {
+			    separator = p_str;
+			} else {
+		            goto opt_error;
+			}
+		    } else {
+		        goto opt_error;
+		    }
+		} else {
+		    goto opt_error;
 		}
+	    } else {
+	        if (ind_point < 1) {
+		    ind_point++;
+		    ind = i;
+	        } else {
+		    fprintf(stderr, "Can't read from multiple input files.\n");
+		    goto opt_error;
+	        }
+	    }
+	    i++;
 	}
 
-	argc -= optind;
-	argv += optind;
-
-	if (argc == 1) {
-		data_filename = argv[0];
-	} else if (argc == 0) {
+	if (ind_point == 1) {
+		data_filename = *(argv+ind);
+	} else if (ind_point == 0) {
 		data_filename = NULL;
 	} else {
 		fprintf(stderr, "Can't read from multiple input files.\n");
-		usage();
-		exit(1);
+		goto opt_error;
 	}
 
 	/* Process the data file */
 	process_file(data_filename);
 	return 0;
+
+opt_error:
+	 usage();
+         exit(1);
 }
